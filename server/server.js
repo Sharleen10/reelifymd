bbe7e4d5c71bf35353125ba7a8ba1b9b8a264a63
Sharleen10 <sharleensshabangu@gmail.com>
2025-04-16 21:35:36 +0200

    testing if th backend url will be able to provide the front end with movies

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,23 +17,84 @@ app.get('/health', (req, res) => {
 
 app.use(cors({
   origin: [
-    'https://reelify-frontend.onrender.com',
-    'http://localhost:3000' // for local development
+    'http://localhost:3000',
+    'https://reelify-frontend.onrender.com'
   ],
   credentials: true
 }));
 
-// API Routes
+// ======================
+// TMDB API Routes
+// ======================
+
+// Trending Content (Movies/TV)
+app.get('/api/trending/:timeWindow', async (req, res, next) => {
+  try {
+    const { timeWindow } = req.params;
+    const url = `https://api.themoviedb.org/3/trending/all/${timeWindow}?api_key=${process.env.TMDB_API_KEY}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Movies Routes
 app.get('/api/movies', async (req, res, next) => {
   try {
-    const { page = 1, year, region, sort_by } = req.query;
+    const { page = 1, year, region, sort_by, with_genres } = req.query;
     let url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
     
     if (year) url += `&year=${year}`;
     if (region) url += `&region=${region}`;
     if (sort_by) url += `&sort_by=${sort_by}`;
+    if (with_genres) url += `&with_genres=${with_genres}`;
     
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/movies/now_playing', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/movies/top_rated', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/movies/upcoming', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/movies/genre/:genreId', async (req, res, next) => {
+  try {
+    const { genreId } = req.params;
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&with_genres=${genreId}&page=${page}`;
     const response = await axios.get(url);
     res.json(response.data);
   } catch (err) {
@@ -82,12 +142,101 @@ app.get('/api/tv', async (req, res, next) => {
   }
 });
 
+app.get('/api/tv/trending/:timeWindow', async (req, res, next) => {
+  try {
+    const { timeWindow } = req.params;
+    const url = `https://api.themoviedb.org/3/trending/tv/${timeWindow}?api_key=${process.env.TMDB_API_KEY}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/top_rated', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/on_the_air', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/airing_today', async (req, res, next) => {
+  try {
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/genre/:genreId', async (req, res, next) => {
+  try {
+    const { genreId } = req.params;
+    const { page = 1 } = req.query;
+    const url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&with_genres=${genreId}&page=${page}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.get('/api/tv/:tvId', async (req, res, next) => {
   try {
     const { tvId } = req.params;
     const url = `https://api.themoviedb.org/3/tv/${tvId}?api_key=${process.env.TMDB_API_KEY}`;
     const response = await axios.get(url);
     res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/:tvId/trailer', async (req, res, next) => {
+  try {
+    const { tvId } = req.params;
+    const url = `https://api.themoviedb.org/3/tv/${tvId}/videos?api_key=${process.env.TMDB_API_KEY}`;
+    const response = await axios.get(url);
+    const trailers = response.data.results.filter(vid => vid.type === "Trailer");
+    res.json(trailers.length > 0 ? trailers[0] : {});
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Genres Routes
+app.get('/api/genres', async (req, res, next) => {
+  try {
+    const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_API_KEY}`;
+    const response = await axios.get(url);
+    res.json(response.data.genres);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/tv/genres', async (req, res, next) => {
+  try {
+    const url = `https://api.themoviedb.org/3/genre/tv/list?api_key=${process.env.TMDB_API_KEY}`;
+    const response = await axios.get(url);
+    res.json(response.data.genres);
   } catch (err) {
     next(err);
   }
